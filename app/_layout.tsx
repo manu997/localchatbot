c26f5596b3a84f2cbe5f674e11ca60e37ea55cfc
manu997 <manuelmorenoasiain@gmail.com>
2025-03-23 16:49:17 +0100

    Simplify navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,10 @@
-import {
-	DarkTheme,
-	DefaultTheme,
-	ThemeProvider,
-} from "@react-navigation/native";
-import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { StatusBar } from "expo-status-bar";
-import { useEffect } from "react";
-import "react-native-reanimated";
 import React from "react";
+import { Stack } from "expo-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-import { useColorScheme } from "@/hooks/useColorScheme";
+import { StatusBar } from "expo-status-bar";
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -21,7 +13,6 @@ SplashScreen.preventAutoHideAsync();
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
-	const colorScheme = useColorScheme();
 	const [loaded] = useFonts({
 		SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
 	});
@@ -38,15 +29,16 @@ export default function RootLayout() {
 
 	return (
 		<QueryClientProvider client={queryClient}>
-			<ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-				<Stack>
-					<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-					<Stack.Screen name="+not-found" />
-					<Stack.Screen name="index" options={{ headerShown: false }} />
-					<Stack.Screen name="chat/index" options={{ headerShown: true }} />
-				</Stack>
-				<StatusBar style="auto" />
-			</ThemeProvider>
+			<Stack screenOptions={{ headerShown: true }}>
+				<Stack.Screen
+					name="index"
+					options={{
+						headerTitle: "Chat con GGUF",
+						headerTitleStyle: { fontWeight: "bold" },
+					}}
+				/>
+			</Stack>
+			<StatusBar style="auto" />
 		</QueryClientProvider>
 	);
 }
